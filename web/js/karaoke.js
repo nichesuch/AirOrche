816634e48c6d84953166a3e7a440680783f9f832
niche.and.such <niche.and.such@gmail.com>
2017-02-05 06:09:50 +0900

    file upload

--- web/js/karaoke.js
+++ web/js/karaoke.js
@@ -13,121 +13,128 @@ KaraokeClass.init = function() {
         demoAudio = document.getElementById('demo-audio');
 
     // file select
-    //fileselect.addEventListener('change', fileSelectHandler, false);
-/*
+    fileselect.addEventListener('change', fileSelectHandler, false);
+
     var xhr = new XMLHttpRequest();
     if (xhr.upload) {
-      // file drop
-      filedrag.addEventListener('dragover', fileDragHover, false);
-      filedrag.addEventListener('dragleave', fileDragHover, false);
-      filedrag.addEventListener('drop', fileSelectHandler, false);
-      filedrag.style.display = 'block';
+        // file drop
+        filedrag.addEventListener('dragover', fileDragHover, false);
+        filedrag.addEventListener('dragleave', fileDragHover, false);
+        filedrag.addEventListener('drop', fileSelectHandler, false);
+        filedrag.style.display = 'block';
     } else {
-      filedrag.style.display = 'none';
+        filedrag.style.display = 'none';
     }
-*/
+
     karaokeEnabled = true;
-/*
-    demoAudio.addEventListener('click', function() {
-      playSound('audio/sing_donguri.m4a')
-    }, false);
-    */
-    playSound('sound/sing_donguri.m4a');
+  /*
+   demoAudio.addEventListener('click', function() {
+   playSound('audio/sing_donguri.m4a')
+   }, false);
+   */
+    // playSound('sound/sing_donguri.m4a');
+
+    // file drag hover
+    function fileDragHover(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        e.target.className = (e.type === 'dragover' ? 'hover' : '');
+    }
+
+    // file selection
+    function fileSelectHandler(e) {
+        // cancel event and hover styling
+        fileDragHover(e);
+
+        var droppedFiles = e.target.files || e.dataTransfer.files;
+
+        var reader = new FileReader();
+
+        reader.onload = function(fileEvent) {
+            var data = fileEvent.target.result;
+            audioFile = data;
+            document.getElementById('filedrag').innerHTML = 'OK';
 
 
-  // plays a file
-  function playSound(url) {
+            // var currentSong = document.getElementById('current-song');
+            // var dv = new jDataView(this.result);
+
+            // "TAG" starts at byte -128 from EOF.
+            // See http://en.wikipedia.org/wiki/ID3
+            // if (dv.getString(3, dv.byteLength - 128) == 'TAG') {
+            //     var title = dv.getString(30, dv.tell());
+            //     var artist = dv.getString(30, dv.tell());
+            //     var album = dv.getString(30, dv.tell());
+            //     var year = dv.getString(4, dv.tell());
+            //     currentSong.innerHTML = 'Playing ' + title + ' by ' + artist;
+            // } else {
+            //     // no ID3v1 data found.
+            //     currentSong.innerHTML = 'Playing';
+            // }
+
+            // options.style.display = 'block';
+        };
+
+        // http://ericbidelman.tumblr.com/post/8343485440/reading-mp3-id3-tags-in-javascript
+        // https://github.com/jDataView/jDataView/blob/master/src/jDataView.js
+
+        reader.readAsArrayBuffer(droppedFiles[0]);
+
+    }
+
+};
+
+KaraokeClass.playSound = function(url) {
     var request = new XMLHttpRequest();
+    var self = this;
 
     request.open('GET', url, true);
     request.responseType = 'arraybuffer';
 
     // Our asynchronous callback
     request.onload = function() {
-      var data = request.response;
-      console.log("test");
-      initAudio(data);
+        var data = request.response;
+        console.log("test");
+        self.initAudio(data);
     };
 
     request.send();
-  }
+};
 
-  // file drag hover
-  function fileDragHover(e) {
-    e.stopPropagation();
-    e.preventDefault();
-    e.target.className = (e.type === 'dragover' ? 'hover' : '');
-  }
-
-  // file selection
-  function fileSelectHandler(e) {
-    // cancel event and hover styling
-    fileDragHover(e);
-
-    var droppedFiles = e.target.files || e.dataTransfer.files;
-
-    var reader = new FileReader();
-
-    reader.onload = function(fileEvent) {
-      var data = fileEvent.target.result;
-      initAudio(data);
-
-      var currentSong = document.getElementById('current-song');
-      var dv = new jDataView(this.result);
-
-      // "TAG" starts at byte -128 from EOF.
-      // See http://en.wikipedia.org/wiki/ID3
-      if (dv.getString(3, dv.byteLength - 128) == 'TAG') {
-        var title = dv.getString(30, dv.tell());
-        var artist = dv.getString(30, dv.tell());
-        var album = dv.getString(30, dv.tell());
-        var year = dv.getString(4, dv.tell());
-        currentSong.innerHTML = 'Playing ' + title + ' by ' + artist;
-      } else {
-        // no ID3v1 data found.
-        currentSong.innerHTML = 'Playing';
-      }
-
-      options.style.display = 'block';
-    };
-
-    // http://ericbidelman.tumblr.com/post/8343485440/reading-mp3-id3-tags-in-javascript
-    // https://github.com/jDataView/jDataView/blob/master/src/jDataView.js
-
-    reader.readAsArrayBuffer(droppedFiles[0]);
-
-  }
-
-  // call initialization file
-  if (window.File && window.FileList && window.FileReader) {
-//    init();
-  } else {
-    alert('Your browser does not support File');
-  }
-
-  var context = new (window.AudioContext || window.webkitAudioContext)();
-  var source;
-  var processor,
+var context = new (window.AudioContext || window.webkitAudioContext)();
+var source;
+var processor,
     filterLowPass,
-    filterHighPass;
-
-  function initAudio(data) {
+    filterHighPass,
+    audioFile;
+KaraokeClass.initAudio = function(data) {
     if (source) source.stop(0);
 
     source = context.createBufferSource();
 
     if (context.decodeAudioData) {
-      context.decodeAudioData(data, function (buffer) {
-        source.buffer = buffer;
-        createAudio();
-      }, function (e) {
-        console.error(e);
-      });
+        context.decodeAudioData(data, function (buffer) {
+            source.buffer = buffer;
+            createAudio();
+        }, function (e) {
+            console.error(e);
+        });
     } else {
-      source.buffer = context.createBuffer(data, false);
-      createAudio();
+        source.buffer = context.createBuffer(data, false);
+        createAudio();
     }
+
+
+
+  // call initialization file
+  if (window.File && window.FileList && window.FileReader) {
+   init();
+  } else {
+    alert('Your browser does not support File');
   }
+
+
+
 
   function createAudio() {
     // create low-pass filter
