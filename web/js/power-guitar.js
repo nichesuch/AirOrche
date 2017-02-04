3dbbba2ffd07c404417b9c0d3eb7ebc9138d439a
niche.and.such <niche.and.such@gmail.com>
2017-02-04 22:27:42 +0900

    guitar

---
+++ web/js/power-guitar.js
@@ -0,0 +1,31 @@
+var PowerGuitar = {
+  source:null
+};
+
+PowerGuitar.play = function() {
+  function playSound(buffer, time) {
+    this.source = context.createBufferSource();
+    this.source.buffer = buffer;
+    this.source.connect(context.destination);
+    if (!this.source.start)
+      this.source.start = this.source.noteOn;
+    this.source.start(time);
+  }
+
+  var guitar = BUFFERS.guitar;
+
+  playSound(guitar, time);
+
+  }
+};
+
+
+PowerGuitar.setPower = function(num) {
+  var x = num;
+  //var x = parseInt(element.value) / parseInt(element.max);
+
+  // Use an equal-power crossfading curve:
+  var gain1 = Math.cos(x * 0.5*Math.PI);
+
+  this.source.gainNode.gain.value = gain1;
+};
