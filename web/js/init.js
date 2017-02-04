985b15b61ad5bade2d97c115b5603af394974f3d
niche.and.such <niche.and.such@gmail.com>
2017-02-04 23:19:13 +0900

    a

--- web/js/init.js
+++ web/js/init.js
@@ -5,10 +5,10 @@ var context = null;
 
 // An object to track the buffers to load {name: path}
 var BUFFERS_TO_LOAD = {
-  type1: 'sound/sing.m4a',
+  type1: 'sound/sing_dongri.m4a',
   type2: 'sound/hihat.wav',
   type3: 'sound/hihat.wav',
-  type4: 'sound/karaoke.m4a',
+  type4: 'sound/dongri.m4a',
   kick: 'sound/kick.wav',
   snare: 'sound/snare.wav',
   hihat: 'sound/hihat.wav',
