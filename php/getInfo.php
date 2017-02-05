33c85783c5333e2949e2097e58d649c562877540
niche.and.such <niche.and.such@gmail.com>
2017-02-05 09:05:00 +0900

    a

---
+++ php/getInfo.php
@@ -0,0 +1,26 @@
+<?php
+
+echo $_POST;
+exit;
+
+$dir = $_POST["dir"];
+if($_FILES["file"]["tem_name"]){
+  list($file_name,$file_type) = explode(".",$_FILES['file']['name']);
+  //ファイル名を日付と時刻にしている。
+  $name = date("YmdHis").".".$file_type;
+  $file = "tmp/".$dir;
+  //ディレクトリを作成してその中にアップロードしている。
+  if(!file_exists($file)){
+    mkdir($file,0755);
+  }
+  if (move_uploaded_file($_FILES['file']['tmp_name'], $file."/".$name)) {
+    chmod($file."/".$name, 0644);
+    var_dump($dir."/".$name);
+    $filename = $file."/".$name;
+    $output = shell_exec("mp4info $filename");
+  }
+}
+
+
+echo "<pre>$output</pre>";
+?>
---
+++ web/js/mp4Info.js
@@ -0,0 +1,23 @@
+Mp4InfoClass.info = function(data) {
+ var oReq = new XMLHttpRequest();
+ oReq.open("POST", "../getInfo.php", true);
+ oReq.onload = function (oEvent) {
+   // Uploaded.
+ };
+
+ var blob = new Blob(['1234'], {type: 'text/plain'});
+
+ oReq.send(blob);
+
+ oReq.onreadystatechange = function() {
+  var result = document.getElementById('result');
+  if (oReq.readyState == 4) { // 通信の完了時
+    if (oReq.status == 200) { // 通信の成功時
+      result.innerHTML = oReq.responseText;
+    }
+  }else{
+    result.innerHTML = "通信中..."
+  }
+ }
+
+};
